f356fb63213517f9920d4a96162e6abcf198b288
Gh0u1L5 <Gh0u1L5@outlook.com>
2022-08-10 00:16:58 +0800

    Create project.

---
+++ BitComet/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$url        = 'https://download.bitcomet.com/achive/BitComet_1.79_setup.exe'
+$packageArgs = @{
+  softwareName  = 'BitComet*'
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  url           = $url
+
+  checksum      = '6860cdfd193b7f483375c2e2a70e1e3ec6601d917e29fb37fb5d8e7539722008'
+  checksumType  = 'sha256'
+
+  silentArgs    = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
