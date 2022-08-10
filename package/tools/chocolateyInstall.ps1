92aabc647d02d90bd32633cb0e2a04a37979458b
Gh0u1L5 <Gh0u1L5@outlook.com>
2022-08-10 22:21:22 +0800

    Create empty project template.

---
+++ package/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$url         = 'PLACEHOLDER';
+$packageArgs = @{
+  softwareName  = 'PLACEHOLDER',
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  url           = $url
+
+  checksum      = 'PLACEHOLDER'
+  checksumType  = 'sha256'
+
+  silentArgs    = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
