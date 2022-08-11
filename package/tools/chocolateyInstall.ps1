7b68d2eca1f7c8e2e40c136a0b480559c17c7747
Headless <headless@example.com>
2022-08-11 03:49:08 +0000

    Bump version

--- package/tools/chocolateyInstall.ps1
+++ package/tools/chocolateyInstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url         = 'https://gethstore.blob.core.windows.net/builds/geth-windows-amd64-1.10.20-8f2416a8.exe';
+$url         = 'https://gethstore.blob.core.windows.net/builds/geth-windows-amd64-1.10.21-67109427.exe'
 $packageArgs = @{
   softwareName  = 'Ethereum*'
   packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
   url           = $url
 
-  checksum      = '97d008dfb7ced6948a5f65e0933ae35f'
+  checksum      = '63ac9db01d2d51f6361a5b223f60688c'
   checksumType  = 'md5'
 
   silentArgs    = '/S'
