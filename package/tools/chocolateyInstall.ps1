471e0b072d51dd4a373aeda9751315258642a825
Headless <headless@example.com>
2025-12-25 02:45:21 +0000

    Bump version

--- package/tools/chocolateyInstall.ps1
+++ package/tools/chocolateyInstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url         = 'https://download.bitcomet.com/achive/BitComet_2.18_setup.exe'
+$url         = 'https://download.bitcomet.com/achive/BitComet_2.19_setup.exe'
 $packageArgs = @{
   softwareName  = 'BitComet*'
   packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
   url           = $url
 
-  checksum      = '8ca4edfe5fc67b21a03c249b918bd18e320dd3df1fd42b1994bc24af3891156e'
+  checksum      = '5454eca1bc17359e2c4ae088134d95fb465c60500f4bde3114277bcb7b77f7bb'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
